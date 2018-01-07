2a583467dc2a4dc5a51e9a6256007fb31c1c8fba
Matthieu <Matthieu@IBARRA_PC>
2018-01-07 23:58:52 +0100

    Cleaning

--- swax-webservice/src/main/java/swax/webservice/entity/User.java
+++ swax-webservice/src/main/java/swax/webservice/entity/User.java
@@ -1,9 +1,5 @@
 package swax.webservice.entity;
-import static javax.persistence.GenerationType.IDENTITY;
-
-import java.io.Serializable;import java.util.ArrayList;import java.util.HashSet;import java.util.List;import java.util.Set;import javax.persistence.Column;
-import javax.persistence.Entity;import javax.persistence.FetchType;import javax.persistence.GeneratedValue;
-import javax.persistence.Id;import javax.persistence.JoinColumn;import javax.persistence.OneToMany;import javax.persistence.Table;import swax.webservice.dto.AlbumDTO;
+import static javax.persistence.GenerationType.IDENTITY;import java.io.Serializable;import java.util.ArrayList;import java.util.List;import javax.persistence.Column;import javax.persistence.Entity;import javax.persistence.FetchType;import javax.persistence.GeneratedValue;import javax.persistence.Id;import javax.persistence.OneToMany;import javax.persistence.Table;import swax.webservice.dto.AlbumDTO;
 @Entity@Table(name="USER")public class User implements Serializable {
 	private static final long serialVersionUID = -7198215437040727852L;
 	@Id	@GeneratedValue(strategy = IDENTITY)	@Column(name="USER_ID", unique = true, nullable = false)	private Integer userId;
--- swax-webservice/src/main/java/swax/webservice/serviceImpl/UserServiceImpl.java
+++ swax-webservice/src/main/java/swax/webservice/serviceImpl/UserServiceImpl.java
@@ -1,11 +1,5 @@
 package swax.webservice.serviceImpl;
-import java.lang.reflect.Array;import java.util.List;import java.util.Random;import javax.annotation.Resource;
-
-import org.springframework.stereotype.Service;
-
-import swax.webservice.dao.IUserDAO;
-import swax.webservice.entity.User;
-import swax.webservice.service.IUserService;
+import java.util.List;import java.util.Random;import javax.annotation.Resource;import org.springframework.stereotype.Service;import swax.webservice.dao.IUserDAO;import swax.webservice.entity.User;import swax.webservice.service.IUserService;
 @Service("userService")public class UserServiceImpl implements IUserService{
 	@Resource(name = "userDAO")	private IUserDAO userDAO = null;
 	@Override	public List<User> findAll() {		return this.userDAO.findAll();	}
